1751d18dc1142bcfd3c693e39499f46e29d5b218
simsj <jimsims.mi@gmail.com>
2014-06-19 10:58:09 -0400

    Corrected comment typos

--- cachematrix.R
+++ cachematrix.R
@@ -16,9 +16,9 @@ cacheSolve <- function(x, ...) {
 
 
 ## the following code creates an example matrix that can be passed 
-## to the fuctions above, and will not generate an error when passed 
+## to the functions above, and will not generate an error when passed 
 ## to the solve function. I found this example matrix on a web page 
-## attributed to John Myles White with the title Quick Review of 
-## Matrix Algebra in R:
+## attributed to John Myles White with the title "Quick Review of 
+## Matrix Algebra in R":
 ##
 m <- matrix(c(0, 2, 1, 0), nrow=2, ncol=2, byrow=TRUE)
